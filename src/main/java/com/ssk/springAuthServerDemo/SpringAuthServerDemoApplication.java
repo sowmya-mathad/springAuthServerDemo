9a211e4a7c582999eedc94a6c628d122e093c1f5
Sowmya <sowmyag719@gmail.com>
2024-04-24 05:59:31 -0400

    initial commit

---
+++ src/main/java/com/ssk/springAuthServerDemo/SpringAuthServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ssk.springAuthServerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthServerDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssk/springAuthServerDemo/SpringAuthServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssk.springAuthServerDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
